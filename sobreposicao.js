9f9df2058c6de363f08ac71cfef7411f9bd04880
antoniojscardoso <antonio.js.cardoso26@gmail.com>
2024-06-04 09:57:11 -0300

    alteração marca d'agua

--- sobreposicao.js
+++ sobreposicao.js
@@ -1,12 +1,18 @@
-const overlay = document.getElementById('overlay');
-const gridItems = document.querySelectorAll('container');
-
-gridItems.forEach(item => {
-  item.addEventListener('click', function() {
-    overlay.style.display = 'block';
-  });
-});
-
-overlay.addEventListener('click', function() {
-  overlay.style.display = 'none';
-});
+function toggleMarcaDagua(imagem) {
+  var marcaDagua = document.querySelector('.marca-dagua');
+  marcaDagua.style.top = imagem.offsetTop + 'px';
+  marcaDagua.style.left = imagem.offsetLeft + 'px';
+  
+  // Verifica se a imagem já possui a marca d'água
+  if (imagem.classList.contains('has-watermark')) {
+      // Remove a marca d'água da imagem
+      imagem.classList.remove('has-watermark');
+      marcaDagua.classList.remove('show');
+      marcaDagua.classList.add('grid'); // Adiciona a classe para mostrar no fundo do grid
+  } else {
+      // Adiciona a marca d'água à imagem
+      imagem.classList.add('has-watermark');
+      marcaDagua.classList.remove('grid'); // Remove a classe para mostrar no fundo do grid
+      marcaDagua.classList.add('show');
+  }
+}
